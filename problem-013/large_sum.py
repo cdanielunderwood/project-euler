bebad635cbfa86fd84013bbbc0417c941558bcb6
Daniel Underwood <99142116+cdanielunderwood@users.noreply.github.com>
2024-11-03 09:32:21 -0600

    Completed solution for problem 13.

---
+++ problem-013/large_sum.py
@@ -0,0 +1,9 @@
+class LargeSum:
+    def __init__(self):
+        pass
+
+    def sum_list_of_ints(self, list):
+        return sum(list)
+
+    def first_10_digits(self, input):
+        return input[0:10]
